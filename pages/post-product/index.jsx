a1121c3a543019a458701f4e429582c194c7e799
florian ricq <florian.ricq@gmail.com>
2022-11-15 13:16:56 +0100

    modif fonctionnement formulaire post product

--- pages/post-product/index.jsx
+++ pages/post-product/index.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import { Box, Button, FormControl, FormLabel, Input, Select } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+} from '@chakra-ui/react'
 
 export default function PostProduct() {
-  const [name, setName] = useState()
-  const [quantity, setQuantity] = useState(1)
-  const [category, setCategory] = useState()
-  const [price, setPrice] = useState()
-  const [description, setDescription] = useState()
-  const [image, setImage] = useState(['new-product.jpeg'])
   const router = useRouter()
 
-  const submitProduct = async () => {
+  const submitProduct = async (e) => {
+    e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form)
+    const name = formData.get('name')
+    const quantity = 1
+    const category = formData.get('category')
+    const price = formData.get('price')
+    const description = formData.get('description')
+    const image = ['new-product.jpeg']
     const response = await fetch('/api/products', {
       method: 'POST',
       body: JSON.stringify({
@@ -26,41 +36,61 @@ export default function PostProduct() {
         'Content-Type': 'application/json',
       },
     })
+    form.reset();
     router.push({
       pathname: '/products',
     })
   }
   return (
-    <Box maxW='600px' w={'100%'} mr='auto' ml='auto'>
-      <FormControl isRequired mb={5}>
-        <FormLabel>Product name</FormLabel>
-        <Input placeholder="Product name" type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)} />
-      </FormControl>
-      <FormControl isRequired mb={5}>
-        <FormLabel>Description</FormLabel>
-        <Input placeholder="Description" type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)} />
-      </FormControl>
-      <FormControl isRequired mb={5}>
-        <FormLabel>Price</FormLabel>
-        <Input placeholder="Price" type="text"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)} />
-      </FormControl>
-      <FormControl isRequired mb={5}>
-        <FormLabel>Categort</FormLabel>
-        <Select placeholder="Select category" name="category"
+    <Box mb={10} mt={10} maxW="600px" w={'100%'} mr="auto" ml="auto">
+      <form onSubmit={submitProduct}>
+        <FormControl isRequired mb={5}>
+          <FormLabel>Nom du produit</FormLabel>
+          <Input
+            placeholder="Nom du produit"
+            type="text"
+            name="name"
+            id="name"
+          />
+        </FormControl>
+        <FormControl isRequired mb={5}>
+          <FormLabel>Description du produit</FormLabel>
+          <Input
+            placeholder="Description du produit"
+            type="text"
+            name="description"
+            id="description"
+          />
+        </FormControl>
+        <FormControl isRequired mb={5}>
+          <FormLabel>Prix du produit</FormLabel>
+          <Input
+            placeholder="Prix du produit"
+            type="number"
+            name="price"
+            id="price"
+          />
+        </FormControl>
+        <FormControl isRequired mb={5}>
+          <FormLabel>Catégorie du produit</FormLabel>
+          <Select
+          placeholder="Catégorie du produit"
+          name="category"
           id="category"
-          onChange={(e) => setCategory(e.target.value)}>
+        >
           <option value="homme">Homme</option>
           <option value="femme">Femme</option>
           <option value="enfant">Enfant</option>
         </Select>
-      </FormControl>
-      <Button onClick={submitProduct} mb={5}>Add product</Button>
+        </FormControl>
+        <Button
+          type="submit"
+          width="full"
+          mt={4}
+        >
+          Ajouter le produit
+        </Button>
+      </form>
     </Box>
   )
 }
